b9763996514dfcceed476c425886f2ef104d397b
Hannah McLaughlin <h@mcla.ug>
2021-12-05 20:05:21 +0000

    add part 2 of day 3

--- day3/day3.py
+++ day3/day3.py
@@ -1,11 +1,8 @@
-def get_gamma_and_epsilon():
+def get_gamma_and_epsilon(values):
     def most_common(values, position):
         num_values = len(values)
         num_ones = sum(int(v[position]) for v in values)
-        return "1" if num_ones > (num_values / 2) else "0"
-
-    with open("input", "r") as f:
-        values = f.read().splitlines()
+        return "1" if num_ones >= (num_values / 2) else "0"
 
     num_bits = len(values[0])
     gamma = int("".join(most_common(values, p) for p in range(num_bits)), 2)
@@ -15,6 +12,28 @@ def get_gamma_and_epsilon():
     return gamma, epsilon
 
 
+def get_rating(values, to_match):
+    num_bits = len(values[0])
+
+    for p in range(num_bits):
+        mask = 1 << (num_bits - p - 1)
+        comp = to_match(values)
+        values = [v for v in values if (int(v, 2) & mask) ^ (comp & mask) == 0]
+
+        if len(values) == 1:
+            break
+
+    assert len(values) == 1
+    return int(values[0], 2)
+
+
 if __name__ == "__main__":
-    gamma, epsilon = get_gamma_and_epsilon()
+    with open("input", "r") as f:
+        values = f.read().splitlines()
+
+    gamma, epsilon = get_gamma_and_epsilon(values)
     print(gamma * epsilon)
+
+    oxygen = get_rating(values, lambda v: get_gamma_and_epsilon(v)[0])
+    co2 = get_rating(values, lambda v: get_gamma_and_epsilon(v)[1])
+    print(oxygen * co2)
